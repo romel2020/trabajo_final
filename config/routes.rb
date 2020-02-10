f4259d6b0ad597d77bcf0df44a132d71dc6920f5
romell <osed.td@gmail.com>
2020-02-10 14:54:07 -0300

    subiendo pagina en construccion

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'quotations/index'
   resources :cusstomers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'quotations#index'
 end
