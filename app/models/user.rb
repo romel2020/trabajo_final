a1bea8e74b29629d72ffff4d62e869dd01667f50
romell <osed.td@gmail.com>
2020-02-24 19:57:04 -0300

    Actualizando el sistema

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          has_many :quotations
          has_many :companies
+
+         def after_sign_in_path_for(resource)
+          stored_location_for(resource) || quotations_index_path
+        end
 end
