93e9deaa272fdfdf8cb37b836be7b247f603a5de
romell <osed.td@gmail.com>
2020-03-11 15:55:40 -0300

    solucionando problemas duales de bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery/dist/jquery
-//= require popper.js//dist/umd/popper
+//= require popper.js/dist/umd/popper
+//= require bootstrap/dist/js/bootstrap
 //= require_tree .
