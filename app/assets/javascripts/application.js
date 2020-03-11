c721d8f2db46b6a3d0bc3f1b3d9d35982335f756
romell <osed.td@gmail.com>
2020-03-11 15:59:29 -0300

    solucionando problemas jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require jquery/dist/jquery
+//= require turbolinks
 //= require popper.js/dist/umd/popper
 //= require bootstrap/dist/js/bootstrap
 //= require_tree .
