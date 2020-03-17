33d353b74a25bca24677eeea7b349fa9c1238a6e
romell <osed.td@gmail.com>
2020-03-17 14:52:31 -0300

    arreglando manifiesto js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery/dist/jquery
-//= require bootstrap/dist/js/bootstrap
 //= require popper.js/dist/umd/popper
+//= require bootstrap/dist/js/bootstrap
 //= require_tree .
